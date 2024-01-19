ec19cf3a766b900f0c070660ac66da3087b0cef0
Isis <isissouzaguimaraes@gmail.com>
2024-01-19 16:01:09 -0300

    tela completa

--- src/conteudo/DeezerSearch.js
+++ src/conteudo/DeezerSearch.js
@@ -4,10 +4,11 @@ import "../App.css";
 const DeezerSearch = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [pesquisarMusicas, setPesquisarMusicas] = useState("artic monkeys");
 
   useEffect(() => {
     const fetchData = async () => {
-      const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=ludmilla";
+      const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${pesquisarMusicas}`;
       const options = {
         method: "GET",
         headers: {
@@ -25,7 +26,7 @@ const DeezerSearch = () => {
         }
 
         const result = await response.json();
-        setData(result.data || []); // Certifique-se de que existe a propriedade 'data'
+        setData(result.data || []);
         setLoading(false);
       } catch (error) {
         console.error("Erro na solicitação:", error.message);
@@ -34,43 +35,69 @@ const DeezerSearch = () => {
     };
 
     fetchData();
-  }, []);
+  }, [pesquisarMusicas]);
 
-  const displayResults = () => {
-    if (loading) {
-      return <p>Carregando...</p>;
-    } else if (data.length > 0) {
-      return data.map((item) => (
-        <div className="App" key={item.id}>
-          <img
-            className="imagem"
-            src={item.album.cover_medium}
-            alt={item.title}
-          />
-          <div className="conteudo">
-            <h3>
-              {item.title} | {Math.floor(item.duration / 60)}:
-              {(item.duration % 60).toString().padStart(2, "0")}
-            </h3>
-            <h3>{item.artist.name}</h3>
-            <h3>
-              <a href={item.preview}>Prévia</a> <br/>
-              <a href={item.link}>Ouvir</a>
-            </h3>
-
-            <h3>{item.tracklist}</h3>
-          </div>
-        </div>
-      ));
-    } else {
-      return <p>Nenhum resultado encontrado.</p>;
-    }
+  const handleSearch = () => {
+    setLoading(true);
+    setData([]);
+    setPesquisarMusicas(document.getElementById("nomeArtista").value);
   };
 
   return (
-    <div>
-      <h2>Resultados da Pesquisa:</h2>
-      {displayResults()}
+    <div className="App">
+      <div className="cabecalho">
+        <h1>
+          Escute uma prévia das suas músicas preferidas ou ouça pelo Deezer
+        </h1>
+
+        <div className="pesquisa">
+          <form>
+            <input
+              id="nomeArtista"
+              className="input"
+              type="text"
+              placeholder="Digite o nome do seu artista."
+            />
+            <br />
+            <button type="button" className="button" onClick={handleSearch}>
+              Buscar
+            </button>
+          </form>
+        </div>
+      </div>
+
+      {/* <h2>Resultados da Pesquisa:</h2> */}
+      <div className="grid-container">
+        {loading ? (
+          <p>Carregando...</p>
+        ) : data.length > 0 ? (
+          data.map((item) => (
+            <div className="conteudo" key={item.id}>
+              <img
+                id="picture"
+                className="imagem"
+                src={item.album.cover_medium}
+                alt={item.title}
+              />
+              <div>
+                <h3 className="title" id="title">
+                  {item.title} | {Math.floor(item.duration / 60)}:
+                  {(item.duration % 60).toString().padStart(2, "0")}
+                </h3>
+                <h3 id="name">{item.artist.name}</h3>
+                <h3>
+                  <a href={item.preview}>Prévia</a> <br />
+                  <a href={item.link}>Ouvir</a>
+                </h3>
+
+                <h3>{item.tracklist}</h3>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Nenhum resultado encontrado.</p>
+        )}
+      </div>
     </div>
   );
 };
